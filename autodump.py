7566618e64a45b3fa1f13a3c09fc3440166132c3
pigajiu <630907423@qq.com>
2024-02-29 17:59:54 +0800

    add args input for download_floder

--- autodump.py
+++ autodump.py
@@ -1,12 +1,22 @@
 from ncmdump import dump
-import os,fnmatch,time
+import os,fnmatch,time,argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', required=False, default=None, help='Brand ID of the operator to create reports for')
+args = parser.parse_args()
+
+path = args.path
 
 print("软件仅供学习交流，请勿用于商业及非法用途，如产生法律纠纷与本人无关。")
 print("------")
-print("请在下方输入网易云音乐下载路径，请确保输入正确，否则无法正常转换。")
-print("如果您不知道路径在哪里，在网易云客户端中点击：设置 --> 下载设置，即可看到下载路径。")
-print("如留空，默认: C:\\CloudMusic\\")
-download_folder = input("下载路径：") or "C:\\CloudMusic\\"
+
+if path:
+    download_folder = path
+else:
+    print("请在下方输入网易云音乐下载路径，请确保输入正确，否则无法正常转换。")
+    print("如果您不知道路径在哪里，在网易云客户端中点击：设置 --> 下载设置，即可看到下载路径。")
+    print("如留空，默认: C:\\CloudMusic\\")
+    download_folder = input("下载路径：") or "C:\\CloudMusic\\"
 os.system('cls')
 waiting = True
 print("当前下载路径：" + download_folder)
